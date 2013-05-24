cc6e57ac6fadd3eccf2190b2ac899c6b76e8c2cf
Cristian Douce <cristian@gravityonmars.com>
2013-05-24 14:19:03 -0300

    Proposal composer layout

--- public/js/site.js
+++
@@ -1,9 +0,0 @@
-(function(window) {
-  $('.tag-typeahead').typeahead({
-    source: ['ada', 'moma', 'tola'],
-    updater: function (item) {
-      alert(item);
-      return item;
-    }
-  });
-})(window);
