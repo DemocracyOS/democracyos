3655195b5dd81e88f8ba6a975784863238567c2a
Cristian Douce <cristian@gravityonmars.com>
2013-12-10 16:42:49 -0300

    [democracyos] - Booting API from boot instead of root

--- index.js
+++ index.js
@@ -2,126 +2,21 @@
  * Module dependencies.
  */
 
-var express = require('express');
 var http = require('http');
+var app = module.exports = require('lib/boot');
+var server = http.createServer(app);
 var balance = require('lib/balance');
-
-/*
- * Create and expose app
- */
-
-var app = exports.app = express();
+var config = require('lib/config');
+var log = require('debug')('democracyos:root');
 
 /**
- * Create and expose server
- */
-
-var server = exports.server = http.createServer(app);
-
-/**
- * Set `app` configure settings
- */
-
-require('lib/setup')(app);
-
-/*
- * Register Models and Launch Mongoose
- * with `app` configuration settings
- */
-
-require('lib/models')(app);
-
-// Each module has its own routes and views
-// receives app as parameter to sincronize
-// custom settings
-
-/*
- * PassportJS Auth Strategies and Routes
- */
-
-require('lib/auth')(app);
-
-/*
- * Twitter card and Facebook card routes
- */
-
-app.use('/twitter-card', require('lib/twitter-card'));
-app.use('/facebook-card', require('lib/facebook-card'));
-
-/*
- * Local signin routes
- */
-
-app.use('/signin', require('lib/signin'));
-
-/*
- * Local signup routes
- */
-
-app.use('/signup', require('lib/signup'));
-
-/*
- * Account settings routes
- */
-
-app.use('/settings', require('lib/settings'));
-
-/*
- * Forgot password routes
- */
-
-app.use('/forgot', require('lib/forgot'));
-
-/**
- * Tag API Service
- */
-
-app.use('/api', require('lib/tag'));
-
-/**
- * Proposal API Service
- */
-
-app.use('/api', require('lib/proposal'));
-
-/**
- * Law API Service
- */
-
-app.use('/api', require('lib/law'));
-
-/**
- * Comment API Service
- */
-
-app.use('/api', require('lib/comment'));
-
-/**
- * Citizen API Service
- */
-
-app.use('/api', require('lib/citizen'));
-
-/**
- * Delegation API Service
- */
-
-app.use('/api', require('lib/delegation'));
-
-/**
- * Mount BootUp
- */
-
-app.use(require('lib/boot'));
-
-/*
- * Start Web server
+ * Launch the server
  */
 
 if (module === require.main) {
   balance(function() {
-    server.listen(app.get('port'), function() {
-      console.log('Application started on port %d', app.get('port'));
+    server.listen(config('privatePort'), function() {
+      log('Application started on port %d', config('privatePort'));
     });
   });
 }
