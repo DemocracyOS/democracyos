07bc86a3c52036b160298e09a49fdc76b42ff651
Jose Fresco <josemariafresco@gmail.com>
2015-01-22 15:33:17 -0300

    [https] Added redirection to HTTPS for requests to HTTP #587

--- index.js
+++ index.js
@@ -3,37 +3,48 @@
  */
 
 var app = module.exports = require('lib/boot');
-var fs = require('fs');
 var http = require('http');
 var https = require('https');
 var balance = require('lib/balance');
 var config = require('lib/config');
-var protocol = config('protocol');
-var ssl = config('ssl');
-var exists = fs.existsSync;
+var fs = require('fs');
 var log = require('debug')('democracyos:root');
 
+var protocol = config('protocol');
+var ssl = config('ssl');
 var secure = 'https' === protocol;
-var port = secure ? ssl.port : config('privatePort');
-var server;
 
+/**
+ * Configure standard server
+ */
+var server = http.createServer(app);
+
+/**
+ * Configure secure server (SSL) if necessary
+ */
+
+var secureServer;
 if (secure) {
   var privateKey = fs.readFileSync(ssl.serverKey, 'utf-8');
   var certificate = fs.readFileSync(ssl.serverCert, 'utf-8');
   var credentials = { key: privateKey, cert: certificate };
-  server = https.createServer(credentials, app);
-} else {
-  server = http.createServer(app);
+  secureServer = https.createServer(credentials, app);
 }
 
 /**
- * Launch the server
+ * Launch the servers
  */
 
 if (module === require.main) {
   balance(function() {
-    server.listen(port, function() {
-      log('Application started on port %d', port);
+    server.listen(config('privatePort'), function() {
+      log('Application started on port %d', config('privatePort'));
     });
+
+    if (secure) {
+      secureServer.listen(ssl.port, function() {
+        log('Secure application started on port %d', ssl.port);
+      });
+    }
   });
 }
