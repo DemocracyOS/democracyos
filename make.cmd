2c51bea7a470d67dde5107f9ce1fe79ce8526ce0
Shakyamuni117te <peter.carter.mail@gmail.com>
2015-04-09 15:21:22 +0100

    Removed superfluous line 13

--- make.cmd
+++ make.cmd
@@ -10,7 +10,6 @@ if NODE_ENV == "" set NODE_ENV="development"
 
 if not "%1" == "run" goto packages
 echo "Starting application..."
-echo "To begin using UKDemocracyOS, navigate to localhost:3000 in a browser window"
 set NODE_PATH=.
 set DEBUG=%DEBUG%
 node index.js
