dee7bd0e2345c57649e21ea7f431a6eea2b75bbf
Jose Fresco <josemariafresco@gmail.com>
2015-04-28 21:08:39 -0300

    Rewrote homepage

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,11 @@ var uglify = require('gulp-uglify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var sourcemaps = require('gulp-sourcemaps');
+var jshint = require('gulp-jshint');
 var mkdirp = require('mkdirp');
+
+// stylus
+var stylint = require('gulp-stylint');
 
 gulp
   .task('serve', function (cb) {
@@ -21,9 +25,15 @@ gulp
   .task('public', function () {
     mkdirp('./public');
   })
-  .task('javascript', ['public'], function () {
+  .task('javascript', ['js:build', 'js:lint'])
+  .task('js:lint', function () {
+    gulp.src('./lib/**/*.js')
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'));
+  })
+  .task('js:build', ['public'], function () {
     var production = false;
-    var b = browserify({ entries: './lib/boot/boot.js', debug: !production });
+    var b = browserify({ entries: './lib/boot/boot.js', debug: !production, verbose: true });
     b = b
       .transform(babelify)
       .bundle()
@@ -39,5 +49,23 @@ gulp
   // .task('watch', function () {
   //   gulp.watch(['lib/**/*.js, lib/**/*.jade'], ['javascript'])
   // })
-  .task('build', ['javascript'])
+  .task('css:build', function () {
+    var production = false;
+    var t = gulp.src('./lib/**/*.styl')
+    if (!production) t = t.pipe(sourcemaps.init());
+    t = t.pipe(stylus())
+    if (!production) t = t.pipe(sourcemaps.write());
+    t = t.pipe(addsrc('./node_modules/**/*.css'))
+      .pipe(concat('app.css'));
+
+    if (production) t = t.pipe(minify());
+    t.pipe(gulp.dest('./public/'));
+
+  })
+  .task('css:lint', function () {
+    gulp.src('./lib/**/*.styl')
+      .pipe(stylint());
+  })
+  .task('css', ['css:build', 'css:lint'])
+  .task('build', ['javascript', 'css'])
   .task('default', ['serve'])
