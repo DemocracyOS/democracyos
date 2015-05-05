2ac46104d37abf3cefb8dfc2a1f4ce78d0800902
Jose Fresco <josemariafresco@gmail.com>
2015-05-05 12:34:36 -0300

    Refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,13 @@
-var exec = require('child_process').exec;
 var gulp = require('gulp');
-var mkdirp = require('mkdirp');
 
 require('./lib/build/js');
 require('./lib/build/css');
+require('./lib/build/public');
+require('./lib/build/serve');
 
 gulp
-  .task('serve', function (cb) {
-    exec('NODE_PATH=. DEBUG=democracyos* node index.js', function (err, stdout, stderr) {
-      console.log(stdout);
-      console.log(stderr);
-      cb(err);
-    });
-  })
-  .task('public', function () {
-    mkdirp('./public');
-  })
   // .task('watch', function () {
   //   gulp.watch(['lib/**/*.js, lib/**/*.jade'], ['javascript'])
   // })
   .task('build', ['javascript', 'css'])
-  .task('default', ['serve'])
+  .task('default', ['build', 'serve'])
