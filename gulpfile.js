fbc0af319b567ac62a26e7bb858920e360a73a59
Jose Fresco <josemariafresco@gmail.com>
2015-05-08 15:48:19 -0300

    [build:assets] Fixed 'assets' task

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 var gulp = require('gulp');
 
-require('./lib/build/js');
+var settings = {
+  public: './public/',
+  clientEntryPoint: './lib/boot/boot.js'
+};
+
+require('./lib/build/js')(settings);
 require('./lib/build/css');
 require('./lib/build/public');
 require('./lib/build/serve');
 require('./lib/build/watch');
-require('./lib/build/assets');
+require('./lib/build/assets')(settings);
+require('./lib/build/clean')(settings);
 
 gulp
-  .task('build', ['javascript', 'css', 'assets'])
+  .task('build', ['js:lint', 'js:build', 'css:lint', 'css:build', 'assets'])
+  .task('dist', ['js:lint', 'js:dist', 'css:lint', 'css:dist', 'assets'])
   .task('default', ['build', 'serve'])
