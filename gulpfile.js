386ae092020825ee268eff2977b3ef3a8cf64bad
Jose Fresco <josemariafresco@gmail.com>
2015-05-21 15:13:14 -0300

    [build] Added watchify #798

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,11 @@ var gulp = require('gulp');
 var settings = {
   public: './public/',
   clientEntryPoint: './lib/boot/boot.js',
-  verbose: false
+  verbose: false,
+  debug: true
 };
 
-require('./lib/build/js')(settings);
+var js = require('./lib/build/js')(settings);
 require('./lib/build/css')(settings);
 require('./lib/build/public')(settings);
 require('./lib/build/serve')(settings);
@@ -15,6 +16,9 @@ require('./lib/build/assets')(settings);
 require('./lib/build/clean')(settings);
 
 gulp
+  .task('js:lint', ['public'], js.lint)
+  .task('js:build', ['public'], js.build)
+  .task('js:watch', js.watch)
   .task('build', [/*'js:lint',*/ 'js:build', /*'css:lint',*/ 'css:build', 'assets'])
   .task('dist', [/*'js:lint',*/ 'js:dist', /*'css:lint',*/ 'css:dist', 'assets'])
   .task('default', ['build', 'serve'])
