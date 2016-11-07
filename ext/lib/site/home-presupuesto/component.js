c479d8eaa366d2c272eec2d05637f4ed9d2c2411
Matías Lescano <mjlescano@protonmail.com>
2016-11-07 12:16:11 -0300

    add voting debug view

--- ext/lib/site/home-presupuesto/component.js
+++ ext/lib/site/home-presupuesto/component.js
@@ -1,9 +1,98 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router'
+import t from 't-component'
+import user from 'lib/site/user/user'
+import userConnector from 'lib/site/connectors/user'
 
-export default class HomePresupuesto extends Component {
+class HomePresupuesto extends Component {
   render () {
     return (
-      <div>Presupuesto</div>
+      <div>
+        {this.props.user.state.fulfilled && <VotingModule />}
+      </div>
+    )
+  }
+}
+
+export default userConnector(HomePresupuesto)
+
+class VotingModule extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      loading: false,
+      canVote: null,
+      message: '',
+      error: ''
+    }
+  }
+
+  componentWillMount () {
+    if (!user.profileIsComplete()) {
+      return this.setState({
+        canVote: false,
+        message: 'Para poder votar debes completar la información de tu perfil haciendo <a href="/signup/complete">click aqui</a>.'
+      })
+    }
+
+    this.setState({loading: true})
+
+    fetch('/ext/api/participatory-budget/status', {
+      credentials: 'same-origin',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then((res) => {
+      if (res.status >= 200 && res.status < 300) return res.json()
+
+      const err = new Error(res.statusText)
+      err.res = res
+      throw err
+    }).then((body) => {
+      this.setState({
+        loading: false,
+        error: '',
+        message: body.results.mensaje_aclaratorio,
+        canVote: body.results.puede_votar
+      })
+    }).catch((err) => {
+      console.error(err)
+      this.setState({
+        loading: false,
+        error: t('modals.error.default')
+      })
+    })
+  }
+
+  render () {
+    if (this.state.loading) return null
+
+    if (this.state.error) {
+      return (
+        <div className='alert alert-danger error' role='alert'>
+          {this.state.error}
+        </div>
+      )
+    }
+
+    return (
+      <div>
+        {this.state.message && (
+          <div
+            className='alert alert-info error'
+            role='alert'
+            dangerouslySetInnerHTML={{__html: this.state.message}} />
+        )}
+        {this.state.canVote && (
+          <Link
+            className='btn btn-primary'
+            to='/ext/api/participatory-budget/vote'>
+            Votar
+          </Link>
+        )}
+      </div>
     )
   }
 }
