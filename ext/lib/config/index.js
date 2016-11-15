3c7c39278706a03b12b60976c63e5df8c40abe80
Matías Lescano <mjlescano@protonmail.com>
2016-11-15 06:22:29 -0300

    add ext config for front-end

--- ext/lib/config/index.js
+++ ext/lib/config/index.js
@@ -1,6 +1,13 @@
 const path = require('path')
 const democracyosConfig = require('democracyos-config')
+const clientConfig = require('lib/config/client')
 
-module.exports = democracyosConfig({
+const extConfig = module.exports = democracyosConfig({
   path: path.join(__dirname, '..', '..', 'config')
+})
+
+clientConfig.ext = clientConfig.ext || {}
+
+extConfig.ext.client.forEach((k) => {
+  clientConfig.ext[k] = extConfig.ext[k]
 })
