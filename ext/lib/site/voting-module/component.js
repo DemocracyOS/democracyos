a96a3b435b81b9ab1e77eee38b820afabb023da5
Matías Lescano <mjlescano@protonmail.com>
2016-11-15 08:02:44 -0300

    update voting messages

--- ext/lib/site/voting-module/component.js
+++ ext/lib/site/voting-module/component.js
@@ -3,28 +3,61 @@ import {Link} from 'react-router'
 import user from 'lib/site/user/user'
 import userConnector from 'lib/site/connectors/user'
 
+/*
+
+mensaje.estado.0 = Invocaci&oacute;n incorrecta.
+mensaje.estado.1 = Se encuentra habilitado para votar.
+mensaje.estado.2 = Ya participaste con este documento.
+mensaje.estado.3 = Lo sentimos, tus datos no se encuentran en el padr&oacute;n de votantes habilitados, \
+pod&eacute;s dirigirte a tu Centro Municipal de Distrito o consultar a: <a href='yoparticipo@rosario.gob.ar'>yoparticipo@rosario.gob.ar</a>.
+mensaje.estado.10 = El período de votación no ha comenzado todavía.
+#El per&iacute;odo de votaci&oacute;n se inicia el 6/11 a las 8:00hs.
+mensaje.estado.11 = Se encuentra en el período de votación.
+mensaje.estado.12 = El período de votación ha finalizado.
+
+ */
+
 class VotingModule extends Component {
   constructor (props) {
     super(props)
 
     this.state = {
       loading: false,
-      canVote: null,
       message: '',
-      error: ''
+      error: null
     }
   }
 
-  componentWillReceiveProps (nextProps) {
-    if (!nextProps.user.state.fulfilled) {
-      this.setState({loading: true})
-      return null
+  componentWillReceiveProps (props) {
+    if (props.user.state.pending) {
+      return this.setState({loading: true})
+    }
+
+    if (props.user.state.rejected) {
+      return this.setState({
+        loading: false,
+        error: null,
+        message: (
+          <Link
+            to={{pathname: '/signin', query: {ref: location.pathname}}}
+            className='btn btn-lg btn-block btn-primary'>
+            Ingresar y Votar
+          </Link>
+        )
+      })
     }
 
     if (!user.profileIsComplete()) {
       return this.setState({
-        canVote: false,
-        message: 'Para poder votar debes completar la información de tu perfil haciendo <a href="/signup/complete">click aqui</a>.'
+        loading: false,
+        error: null,
+        message: (
+          <Link
+            to={{pathname: '/signup/complete', query: {ref: location.pathname}}}
+            className='btn btn-lg btn-block btn-primary'>
+            Votar
+          </Link>
+        )
       })
     }
 
@@ -43,50 +76,80 @@ class VotingModule extends Component {
       err.res = res
       throw err
     }).then((body) => {
+      const codigo = body.results.codigo_mensaje
+
+      if (!codigo) {
+        const err = new Error('Código inválido.')
+        err.body = body
+        throw err
+      }
+
+      let msg
+
+      if (codigo === 1 || codigo === 11) {
+        msg = (
+          <Link
+            to='/ext/api/participatory-budget/vote'
+            className='btn btn-lg btn-block btn-primary'>
+            Votar
+          </Link>
+        )
+      } else if (codigo === 2) {
+        msg = (
+          <div className='voting-success' role='alert'>
+            <h1>¡Gracias por tu Voto!</h1>
+            <span>Compartilo con tus vecinos</span>
+            <div className='social-links'>
+              <a href='#' className='fb'></a>
+              <a href='#' className='tw'></a>
+              <a href='#' className='msj'></a>
+            </div>
+          </div>
+        )
+      } else if (codigo === 3) {
+        msg = (
+          <div className='alert alert-info' role='alert'>
+            Lo sentimos, tus datos no se encuentran en el padrón de votantes habilitados. Podés dirigirte a tu Centro Municipal de Distrito o consultar a: <a href='mailto:yoparticipo@rosario.gob.ar'>yoparticipo@rosario.gob.ar</a>.
+          </div>
+        )
+      } else {
+        console.log(body)
+        msg = (
+          <Link
+            to='/ext/api/participatory-budget/vote'
+            className='btn btn-lg btn-block btn-primary'>
+            Votar
+          </Link>
+        )
+      }
+
       this.setState({
         loading: false,
-        error: '',
-        message: body.results.mensaje_aclaratorio,
-        canVote: body.results.puede_votar
+        error: null,
+        message: msg
       })
     }).catch((err) => {
       console.error(err)
       this.setState({
         loading: false,
-        error: '¡Lo sentimos! Hubo un error verificando el estado de tu votación.'
+        message: null,
+        error: (
+          <div className='alert alert-danger error' role='alert'>
+            ¡Lo sentimos! Hubo un error verificando tu perfil de votación. Volvé a probar mas tarde o contactanos en <a href='mailto:participa@rosario.gob.ar'>participa@rosario.gob.ar</a>
+          </div>
+        )
       })
     })
   }
 
   render () {
-    if (!this.props.user.state.fulfilled) return null
     if (this.state.loading) return null
-
-    if (this.state.error) {
-      return (
-        <div className='ext-voting-module'>
-          <div className='alert alert-danger error' role='alert'>
-            {this.state.error}
-          </div>
-        </div>
-      )
-    }
+    if (!this.state.error && !this.state.message) return null
 
     return (
       <div className='ext-voting-module'>
-        {this.state.message && (
-          <div
-            className='alert alert-info error'
-            role='alert'
-            dangerouslySetInnerHTML={{__html: this.state.message}} />
-        )}
-        {this.state.canVote && (
-          <Link
-            className='btn btn-primary'
-            to='/ext/api/participatory-budget/vote'>
-            Votar
-          </Link>
-        )}
+        {this.state.error}
+        {this.state.message}
       </div>
     )
   }
