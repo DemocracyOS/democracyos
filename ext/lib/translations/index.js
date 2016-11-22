ae31b25a399f3ae709eb343432475a3e65453488
Matías Lescano <mjlescano@protonmail.com>
2016-11-22 16:13:21 -0300

    update arguments to comments wording Close #58

--- ext/lib/translations/index.js
+++ ext/lib/translations/index.js
@@ -1,6 +1,3 @@
 const translations = require('lib/translations')
 
-Object.assign(translations.es, {
-  "comments.arguments": "Comentarios",
-  "comments.arguments.reply": "Responder a este comentario"
-})
+Object.assign(translations.es, require('./lib/es.json'))
