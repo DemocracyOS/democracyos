d17dc6a6ab85b85244748865dda84e25ce5c6a51
Francisco P <francisco@democracyos.io>
2017-03-13 12:42:43 -0300

    fix owner migration

--- migrations/1487113137271-add-topic-owner.js
+++ migrations/1487113137271-add-topic-owner.js
@@ -7,32 +7,46 @@ const modelsReady = require('lib/models').ready
 const mapPromises = (fn) => (array) => Promise.all(array.map(fn))
 const config = require('lib/config')
 
+function staff (topic) {
+  return User.findOne({ email: config.staff[0] })
+    .then((staff) => ({ topic: topic._id, owner: staff._id }))
+}
+
+function forumById (topic) {
+  return Forum.collection
+    .findOne({ _id: topic.forum })
+}
+
+function findOwner (topic) {
+  return (forum) => forum && forum.owner
+    ? Promise.resolve({ topic: topic._id, owner: forum.owner })
+    : staff(topic)
+}
+
+function setOwner (topic, owner) {
+  return new Promise((resolve, reject) => {
+    Topic.collection
+      .findOneAndUpdate({ _id: topic }, { $set: { owner } }, function (err, r) {
+        if (err) {
+          return reject(err)
+        }
+        resolve(1)
+      })
+  })
+}
+
 exports.up = function up (done) {
   modelsReady()
-    .then(() => Topic.collection.find({}).toArray())
+    .then(() => Topic.collection.find({ owner: { $exists: false } }).toArray())
     .then(mapPromises(function (topic) {
-      if (topic.owner) return Promise.resolve(0)
-      const updateOwner = (owner) => Topic.collection.findOneAndUpdate({ _id: topic._id }, {
-        $set: {
-          owner: owner
-        }
-      })
-
-      return Forum.collection.findOne({ _id: topic.forum }).then((forum) => {
-        if (forum && forum.owner) {
-          return updateOwner(forum.owner)
-        } else {
-          return User.findOne({ email: config.staff[0] })
-            .then((staff) => {
-              if (!staff) throw new Error('Cant assign owner')
-              return updateOwner(staff)
-            })
-        }
-      })
+      return forumById(topic)
+        .then(findOwner(topic))
+    }))
+    .then(mapPromises(function (result) {
+      return setOwner(result.topic, result.owner)
     }))
     .then(function (results) {
-      const total = results.filter((v) => !!v).length
-      console.log(`add topics owner from ${total} topics succeded.`)
+      console.log(`add topics owner from ${results.length} topics succeded.`)
       done()
     })
     .catch(function (err) {
