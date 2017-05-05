dc6a88f90fb5f6663e14369bd71c583d27ceeaa1
Matías Lescano <mjlescano@protonmail.com>
2017-05-05 12:37:39 -0300

    override facebook signin link

--- ext/lib/site/auth-facebook/auth-facebook.js
+++ ext/lib/site/auth-facebook/auth-facebook.js
@@ -1,6 +1,9 @@
 import 'lib/boot/routes'
 import router from 'lib/site/boot/router'
+import BtnFacebook from 'lib/site/sign-in/btn-facebook'
 import Confirm from './confirm/component'
+
+BtnFacebook.defaultProps.action = '/auth/facebook/confirm'
 
 router.childRoutes.unshift({
   path: '/auth/facebook/confirm/authorize',
