5c8763c5d4439fed181f12c78224dd2a7ee0467c
Fran <francisco@democracyos.io>
2017-08-22 16:24:10 -0300

    add blacklist migration

--- migrations/1503424667703-add-blacklist-email.js
+++ migrations/1503424667703-add-blacklist-email.js
@@ -14,7 +14,6 @@ const mapPromises = (fn) => (array) => Promise.all(array.map(fn))
 
 exports.up = function up (done) {
   dbReady()
-    .then(() => config.blackListEmails ? Promise.resolve() : Promise.reject())
     .then(() => User.collection.find({ email: { $in: config.blackListEmails.map((d) => new RegExp(`@${d}*$`)) } }).toArray())
     .then((users) => User.collection.deleteMany({ email: { $in: config.blackListEmails.map((d) => new RegExp(`@${d}*$`)) } })
       .then(() => Vote.collection.deleteMany({ author: { $in: users.map((u) => u._id) } })))
@@ -22,10 +21,9 @@ exports.up = function up (done) {
     .then(mapPromises((topic) => {
       return calcResult(topic)
       .then(({ count, results }) => {
-        const closed = topic.closingAt && topic.closingAt.getTime() < Date.now()
         const action = {
-          count: closed ? topic.action.count : count,
-          results: closed ? topic.action.results : results,
+          count: count,
+          results: results,
           method: topic.action.method
         }
         return Topic.collection.findOneAndUpdate({ _id: topic._id }, { $set: { 'action': action } })
