34e8fa047a1cc3f054a773b7251d98c91704c029
trixtar <rita@democracyos.io>
2017-09-19 14:24:31 -0300

    more filters

--- ext/lib/site/home-presupuesto/filters-navbar/component.js
+++ ext/lib/site/home-presupuesto/filters-navbar/component.js
@@ -18,6 +18,16 @@ class FiltersNavbar extends Component {
           oeste: false,
           sudoeste: false,
           sur: false
+        },
+        año: {
+          proyectos2015: false,
+          proyectos2016: false,
+          proyectos2017: false
+        },
+        estado: {
+          proyectados: false,
+          enEjecucion: false,
+          finalizados: false
         }
       },
 
@@ -33,6 +43,16 @@ class FiltersNavbar extends Component {
           oeste: false,
           sudoeste: false,
           sur: false
+        },
+        año: {
+          proyectos2015: false,
+          proyectos2016: false,
+          proyectos2017: false
+        },
+        estado: {
+          proyectados: false,
+          enEjecucion: false,
+          finalizados: false
         }
       },
 
@@ -49,11 +69,11 @@ class FiltersNavbar extends Component {
   handleCheckboxChange = (select) => (e) => {
     const target = e.target
     const value = target.type === 'checkbox' ? target.checked : target.value
-    const name = target.name
+    const id = target.id
 
     let selectFilters = this.state.selectFilters
 
-    selectFilters[select][name] = value
+    selectFilters[select][id] = value
 
     this.setState({
       selectFilters: selectFilters
@@ -85,20 +105,25 @@ class FiltersNavbar extends Component {
   render () {
     return (
     <nav>
+
+      <div className='pp-stage'>
+        Seguimiento de proyectos
+      </div>
+
       <div className='filter'>
         <button
           type='button'
           id="filtro-edad"
           className = 'btn btn-md btn-outline-primary'>
-          Filtro por edad <span>▾</span>
+          Rango de edad <span>▾</span>
           <div className='filter-dropdown' id="opciones-edad">
             <div className='filter-options'>
               <label htmlFor='adultos'>
-              <input onChange={this.handleCheckboxChange('edad')} type='checkbox' name='adultos' checked={this.state.selectFilters.edad.adultos} />
+              <input onChange={this.handleCheckboxChange('edad')} type='checkbox' id='adultos' name='edad' checked={this.state.selectFilters.edad.adultos} />
               Proyecto adultos
               </label>
               <label htmlFor='jovenes'>
-              <input onChange={this.handleCheckboxChange('edad')} type='checkbox' name='jovenes' checked={this.state.selectFilters.edad.jovenes} />
+              <input onChange={this.handleCheckboxChange('edad')} type='checkbox' id='jovenes' name='edad' checked={this.state.selectFilters.edad.jovenes} />
               Proyecto jóvenes
               </label>
             </div>
@@ -119,34 +144,92 @@ class FiltersNavbar extends Component {
           // onClick={() => onChange(d)}
           // className={`btn btn-md btn-outline-primary${isActive}`}>
           className = 'btn btn-md btn-outline-primary'>
-          Filtro por distrito <span>▾</span>
+          Distrito <span>▾</span>
           <div className='filter-dropdown' id="opciones-distrito">
             <div className='filter-options'>
               <label htmlFor='centro'>
-                <input onChange={this.handleCheckboxChange('distrito')} type='checkbox' name='centro' checked={this.state.selectFilters.distrito.centro} />
+                <input onChange={this.handleCheckboxChange('distrito')} type='checkbox' id='centro' name='distrito' checked={this.state.selectFilters.distrito.centro} />
               Centro
               </label>
               <label htmlFor='noroeste'>
-                <input onChange={this.handleCheckboxChange('distrito')} type='checkbox' name='noroeste' checked={this.state.selectFilters.distrito.noroeste} />
+                <input onChange={this.handleCheckboxChange('distrito')} type='checkbox' id='noroeste' name='distrito' checked={this.state.selectFilters.distrito.noroeste} />
               Noroeste
               </label>
               <label htmlFor='norte'>
-                <input onChange={this.handleCheckboxChange('distrito')} type='checkbox' name='norte' checked={this.state.selectFilters.distrito.norte} />
+                <input onChange={this.handleCheckboxChange('distrito')} type='checkbox' id='norte' name='distrito' checked={this.state.selectFilters.distrito.norte} />
               Norte
               </label>
             </div>
             <div className='filter-options'>
               <label htmlFor='oeste'>
-                <input onChange={this.handleCheckboxChange('distrito')} type='checkbox' name='oeste' checked={this.state.selectFilters.distrito.oeste} />
+                <input onChange={this.handleCheckboxChange('distrito')} type='checkbox' id='oeste' name='distrito' checked={this.state.selectFilters.distrito.oeste} />
               Oeste
               </label>
               <label htmlFor='sudoeste'>
-                <input onChange={this.handleCheckboxChange('distrito')} type='checkbox' name='sudoeste' checked={this.state.selectFilters.distrito.sudoeste} />
+                <input onChange={this.handleCheckboxChange('distrito')} type='checkbox' id='sudoeste' name='distrito' checked={this.state.selectFilters.distrito.sudoeste} />
               Sudoeste
               </label>
               <label htmlFor='sur'>
-                <input onChange={this.handleCheckboxChange('distrito')} type='checkbox' name='sur' checked={this.state.selectFilters.distrito.sur} />
+                <input onChange={this.handleCheckboxChange('distrito')} type='checkbox' id='sur' name='distrito' checked={this.state.selectFilters.distrito.sur} />
               Sur
+              </label>
+            </div>
+            <div className='dropdown-actions'>
+              <a className='cancelar' onClick={this.cancelApplyFilters}>Cancelar</a>
+              <a className='aplicar' onClick={this.confirmApplyFilters}>Aplicar</a>
+            </div>
+          </div>
+        </button>
+      </div>
+
+      <div className='filter'>
+        <button
+          type='button'
+          id="filtro-edad"
+          className = 'btn btn-md btn-outline-primary'>
+          Año <span>▾</span>
+          <div className='filter-dropdown' id="opciones-edad">
+            <div className='filter-options'>
+              <label htmlFor='proyectos2015'>
+              <input onChange={this.handleCheckboxChange('año')} type='checkbox' id='proyectos2015' name='año' checked={this.state.selectFilters.año.proyectos2015} />
+              2015
+              </label>
+              <label htmlFor='proyectos2016'>
+              <input onChange={this.handleCheckboxChange('año')} type='checkbox' id='proyectos2016' name='año' checked={this.state.selectFilters.año.proyectos2016} />
+              2016
+              </label>
+              <label htmlFor='proyectos2017'>
+              <input onChange={this.handleCheckboxChange('año')} type='checkbox' id='proyectos2017' name='año' checked={this.state.selectFilters.año.proyectos2017} />
+              2017
+              </label>
+            </div>
+            <div className='dropdown-actions'>
+              <a className='cancelar' onClick={this.cancelApplyFilters}>Cancelar</a>
+              <a className='aplicar' onClick={this.confirmApplyFilters}>Aplicar</a>
+            </div>
+          </div>
+        </button>
+      </div>
+
+      <div className='filter'>
+        <button
+          type='button'
+          id="filtro-edad"
+          className = 'btn btn-md btn-outline-primary'>
+          Estado <span>▾</span>
+          <div className='filter-dropdown' id="opciones-edad">
+            <div className='filter-options'>
+              <label htmlFor='proyectados'>
+              <input onChange={this.handleCheckboxChange('estado')} type='checkbox' id='proyectados' name='estado' checked={this.state.selectFilters.estado.proyectados} />
+              Proyectados
+              </label>
+              <label htmlFor='en-ejecucion'>
+              <input onChange={this.handleCheckboxChange('estado')} type='checkbox' id='en-ejecucion' name='estado' checked={this.state.selectFilters.estado.enEjecucion} />
+              En ejecución
+              </label>
+              <label htmlFor='finalizados'>
+              <input onChange={this.handleCheckboxChange('estado')} type='checkbox' id='finalizados' name='estado' checked={this.state.selectFilters.estado.finalizados} />
+              Finalizados
               </label>
             </div>
             <div className='dropdown-actions'>
