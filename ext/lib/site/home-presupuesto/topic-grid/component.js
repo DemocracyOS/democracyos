e659a8814d0b4d0011f0976003df4f5c8f69dca9
florlafuente <lafuente.florencia@gmail.com>
2017-09-21 15:56:53 -0300

    adds  3  divs in topic grid

--- ext/lib/site/home-presupuesto/topic-grid/component.js
+++ ext/lib/site/home-presupuesto/topic-grid/component.js
@@ -12,14 +12,33 @@ export default class TopicGrid extends Component {
                 {this.props.districts && this.props.districts.length > 0 && (
                     this.props.districts.map((district, i) => 
                         <div key={i} className='topics-section'>
-                            <h2 className='topics-section-container topics-section-title'> 
-                                {district.title} 
+                            <h2 className='topics-section-container topics-section-title topic-title-distrito'> 
+                                    {district.title} 
                             </h2>
                             <div className='topics-container'>
                                 {this.props.loading && <div className='loader' />}
-                                {district.topics.map((topic)=> 
-                                    <TopicCard key={topic.id} topic={topic} />
-                                )}
+                                {district.topics.map((topic)=> {
+                                    if (topic.edad !== 'joven' && topic.attrs.area === '0'){
+                                    return <TopicCard key={topic.id} topic={topic} />}
+                                })}
+                            </div>
+                            <h2 className='topics-section-container topics-section-title topic-title-area'> 
+                                    Area Barrial
+                            </h2>
+                            <div className='topics-container'>
+                                {district.topics.map((topic)=> {
+                                    if (topic.edad !== 'joven' && topic.attrs.area !== '0'){
+                                    return <TopicCard key={topic.id} topic={topic} />}
+                                })}
+                            </div>
+                            <h2 className='topics-section-container topics-section-title topic-title-joven'> 
+                                    Jóvenes
+                            </h2>
+                            <div className='topics-container'>
+                                {district.topics.map((topic)=> {
+                                    if (topic.edad === 'joven'){
+                                    return <TopicCard key={topic.id} topic={topic} />}
+                                })}
                             </div>
                         </div>
                     )
