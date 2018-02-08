7b50189cf94e56548a6d1c3fbb034735ff4b5e9a
Guillermo Croppi <guillermocroppi@gmail.com>
2018-02-08 15:00:56 -0300

    Added reaction rules List, Create, Edit.

--- cms/components/admin.js
+++ cms/components/admin.js
@@ -6,13 +6,14 @@ import RestClient from '../../client/rest-client'
 import CustomRoutes from '../../client/custom-routes'
 import Dashboard from './dashboard'
 import Navbar from './navbar'
+import { ReactionRuleList, ReactionRuleCreate, ReactionRuleEdit } from '../../reactions/components/reaction-rule'
 import { PostList, PostCreate, PostEdit } from './posts'
 
 export default () => (
   <Admin menu={Navbar} title='Democracy OS' restClient={RestClient} customRoutes={CustomRoutes} history={history} >
     <Resource name='posts' list={PostList} create={PostCreate} edit={PostEdit} remove={Delete} />
     <Resource name='settings' list={PostList} />
-    <Resource name='reaction-rule' list={PostList} />
+    <Resource name='reaction-rule' options={{ label: 'Reaction Rules' }} list={ReactionRuleList} />
     <Resource name='users' list={PostList} />
   </Admin>
 )
---
+++ reactions/components/reaction-rule.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Datagrid, TextField, Create, Edit, SimpleForm, TextInput, SelectInput, DateInput, DateField, EditButton, DeleteButton, List, NumberInput } from 'admin-on-rest'
+const METHODS = require('../enum/methods')
+// import BookIcon from 'material-ui/svg-icons/action/book'
+// export const PostIcon = BookIcon
+
+const getChoices = () => {
+  let choicesArray = []
+  METHODS.forEach((method) => {
+    choicesArray.push({
+      id: method,
+      name: method
+    })
+  })
+  return choicesArray
+}
+const minClosingDate = () => {
+  let date = new Date()
+  date.setDate(date.getDate() + 1)
+  return date
+}
+
+export const ReactionRuleList = (props) => (
+  <List {...props} title="List of reaction rules">
+    <Datagrid>
+      <TextField source='name' label="Regla" style={{ fontWeight: 'bold' }} />
+      <TextField source='method' label="Reaction" style={{ textTransform: 'capitalize' }} />
+      <EditButton style={{ textAlign: 'center' }} />
+      <DeleteButton style={{ textAlign: 'center' }} />
+    </Datagrid>
+  </List>
+)
+
+export const ReactionRuleCreate = (props) => (
+  <Create {...props} title="Create a reaction rule">
+    <SimpleForm>
+      <TextInput source='name' label="Name your rule" />
+      <SelectInput source="method" label="Type of Reaction" choices={getChoices()} />
+      <NumberInput source="limit" step={1} />
+      <DateInput label='Opening date' source='startingDate' defaultValue={new Date()} options={{ minDate: new Date() }} />
+      <DateInput label='Closing date' source='closingDate' defaultValue={minClosingDate()} options={{ minDate: minClosingDate() }} />
+    </SimpleForm>
+  </Create>
+)
+
+export const ReactionRuleEdit = (props) => (
+  <Edit {...props}>
+    <SimpleForm>
+      <TextInput source='name' label="Name your rule" />
+      <TextField source="method" label="Type of Reaction" />
+      <NumberInput source="limit" step={1} />
+      <DateField label='Opening date' source='startingDate'/>
+      <DateInput label='Closing date' source='closingDate' options={{ minDate: minClosingDate() }} />
+    </SimpleForm>
+  </Edit>
+)
