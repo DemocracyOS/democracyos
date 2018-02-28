2eddfc47b7cb4ff4cd97cce10b2712480334111d
florlafuente <lafuente.florencia@gmail.com>
2018-02-28 13:15:40 -0300

    Update client folder structure

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,11 @@ import Head from 'next/head'
 import Router from 'next/router'
 import Link from 'next/link'
 import { NextAuth } from 'next-auth-client'
+import Page from '../client/site/components/page'
 import Header from '../client/site/components/header'
 import PostGrid from '../client/site/components/post-grid'
 
-export default class extends React.Component {
-  static async getInitialProps ({ req }) {
-    const res = await fetch('http://localhost:3000/api/v1.0/settings')
-    const settings = await res.json()
-    return {
-      session: await NextAuth.init({ req }),
-      settings: settings
-    }
-  }
-
+export default class extends Page {
   constructor (props) {
     super(props)
     this.state = {
