a432714096bf450818d3ed98084c41781a1f7894
florlafuente <lafuente.florencia@gmail.com>
2018-03-01 07:29:28 -0300

    Add post page

--- pages/post.js
+++ pages/post.js
@@ -4,11 +4,24 @@ import Head from '../client/site/components/head'
 import Header from '../client/site/components/header'
 
 export default class extends Page {
+  constructor (props) {
+    super(props)
+    this.state = {
+      post: null
+    }
+  }
+
+  async componentDidMount () {
+    const post = await fetch(`/api/v1.0/posts/${this.props.url.query.id}`)
+    console.log(post)
+  }
+
   render () {
     return (
       <div className='container'>
         <Head {...this.props} />
         <Header settings={this.props.settings} user={this.props.session.user} />
+        {console.log(this.state.post)}
         <p>
           Hola, soy posts.
         </p>
