40f2c5200a3ba8b98cc90ca7e51dab007ab4ce2e
florlafuente <lafuente.florencia@gmail.com>
2018-03-06 16:13:04 -0300

    Update filter by date function

--- client/site/components/post-grid.js
+++ client/site/components/post-grid.js
@@ -31,7 +31,7 @@ export default class PostGrid extends React.Component {
 
   onScroll = () => {
     if (
-      (window.innerHeight + window.scrollY) >= (document.body.offsetHeight ) &&
+      (window.innerHeight + window.scrollY) >= (document.body.offsetHeight) &&
       this.state.posts
     ) {
       this.handlePagination()
