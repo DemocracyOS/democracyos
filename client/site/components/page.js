9ba3b43182be5eee7b2d9b1360a746af4f6d39b6
florlafuente <lafuente.florencia@gmail.com>
2018-03-26 16:01:12 -0300

    Set locale in index

--- client/site/components/page.js
+++ client/site/components/page.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { NextAuth } from 'next-auth-client'
+import accepts from 'accepts'
+const locales = ['en', 'es']
 
 export default class extends React.Component {
   static async getInitialProps ({ req }) {
@@ -7,7 +9,8 @@ export default class extends React.Component {
     const settings = await res.json()
     return {
       session: await NextAuth.init({ req }),
-      settings: settings
+      settings: settings,
+      locale: await accepts(req).language(locales)
     }
   }
 }
