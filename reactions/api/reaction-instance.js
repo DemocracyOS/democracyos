a4c0a545075cc249afa0b5baa2fb879fd8dc2c29
Guillermo Croppi <guillermocroppi@gmail.com>
2018-04-24 17:58:07 -0300

    Auth for reaction instance, rule and vote

--- reactions/api/reaction-instance.js
+++ reactions/api/reaction-instance.js
@@ -9,61 +9,77 @@ const ReactionInstance = require('../db-api/reaction-instance')
 const { log } = require('../../main/logger')
 // Requires CRUD apis
 const router = express.Router()
+const { isLoggedIn, isAdmin } = require('../../services/users')
 
 router.route('/')
   // POST route
-  .post(async (req, res, next) => {
-    try {
-      const newReactionInstance = await ReactionInstance.create(req.body)
-      res.status(CREATED).json({
-        data: newReactionInstance
-      })
-    } catch (err) {
-      next(err)
-    }
-  })
+  .post(
+    isLoggedIn,
+    isAdmin,
+    async (req, res, next) => {
+      try {
+        const newReactionInstance = await ReactionInstance.create(req.body)
+        res.status(CREATED).json({
+          data: newReactionInstance
+        })
+      } catch (err) {
+        next(err)
+      }
+    })
   // GET reaction-instances
-  .get(async (req, res, next) => {
-    try {
-      const results = await ReactionInstance.list({ limit: req.query.limit, page: req.query.page })
+  .get(
+    isLoggedIn,
+    isAdmin,
+    async (req, res, next) => {
+      try {
+        const results = await ReactionInstance.list({ limit: req.query.limit, page: req.query.page })
 
-      res.status(OK).json({
-        results: results.docs,
-        pagination: {
-          count: results.total,
-          page: results.page,
-          limit: results.limit
-        }
-      })
-    } catch (err) {
-      next(err)
-    }
-  })
+        res.status(OK).json({
+          results: results.docs,
+          pagination: {
+            count: results.total,
+            page: results.page,
+            limit: results.limit
+          }
+        })
+      } catch (err) {
+        next(err)
+      }
+    })
 
 router.route('/:id')
-  .get(async (req, res, next) => {
-    try {
-      const user = await ReactionInstance.get(req.params.id)
-      res.status(OK).json(user)
-    } catch (err) {
-      next(err)
-    }
-  })
-  .put(async (req, res, next) => {
-    try {
-      const updatedReactionInstance = await ReactionInstance.update({ id: req.params.id, reactionInstance: req.body })
-      res.status(OK).json(updatedReactionInstance)
-    } catch (err) {
-      next(err)
-    }
-  })
-  .delete(async (req, res, next) => {
-    try {
-      await ReactionInstance.remove(req.params.id)
-      res.status(OK).json({ id: req.params.id })
-    } catch (err) {
-      next(err)
-    }
-  })
+  .get(
+    isLoggedIn,
+    isAdmin,
+    async (req, res, next) => {
+      try {
+        const user = await ReactionInstance.get(req.params.id)
+        res.status(OK).json(user)
+      } catch (err) {
+        next(err)
+      }
+    })
+  .put(
+    isLoggedIn,
+    isAdmin,
+    async (req, res, next) => {
+      try {
+        const updatedReactionInstance = await ReactionInstance.update({ id: req.params.id, reactionInstance: req.body })
+        res.status(OK).json(updatedReactionInstance)
+      } catch (err) {
+        next(err)
+      }
+    })
+  .delete(
+    isLoggedIn,
+    isAdmin,
+    async (req, res, next) => {
+      try {
+        await ReactionInstance.remove(req.params.id)
+        res.status(OK).json({ id: req.params.id })
+      } catch (err) {
+        next(err)
+      }
+    })
 
 module.exports = router
--- reactions/api/reaction-vote.js
+++ reactions/api/reaction-vote.js
@@ -9,60 +9,76 @@ const ReactionVote = require('../db-api/reaction-vote')
 const { log } = require('../../main/logger')
 // Requires CRUD apis
 const router = express.Router()
+const { isLoggedIn, isAdmin } = require('../../services/users')
 
 router.route('/')
   // POST route
-  .post(async (req, res, next) => {
-    try {
-      const newReactionVote = await ReactionVote.create(req.body)
-      res.status(CREATED).json({
-        data: newReactionVote
-      })
-    } catch (err) {
-      next(err)
-    }
-  })
+  .post(
+    isLoggedIn,
+    isAdmin,
+    async (req, res, next) => {
+      try {
+        const newReactionVote = await ReactionVote.create(req.body)
+        res.status(CREATED).json({
+          data: newReactionVote
+        })
+      } catch (err) {
+        next(err)
+      }
+    })
   // GET reaction-vote
-  .get(async (req, res, next) => {
-    try {
-      const results = await ReactionVote.list({ limit: req.query.limit, page: req.query.page, ids: req.query.ids })
-      res.status(OK).json({
-        results: results.docs,
-        pagination: {
-          count: results.total,
-          page: results.page,
-          limit: results.limit
-        }
-      })
-    } catch (err) {
-      next(err)
-    }
-  })
+  .get(
+    isLoggedIn,
+    isAdmin,
+    async (req, res, next) => {
+      try {
+        const results = await ReactionVote.list({ limit: req.query.limit, page: req.query.page, ids: req.query.ids })
+        res.status(OK).json({
+          results: results.docs,
+          pagination: {
+            count: results.total,
+            page: results.page,
+            limit: results.limit
+          }
+        })
+      } catch (err) {
+        next(err)
+      }
+    })
 
 router.route('/:id')
-  .get(async (req, res, next) => {
-    try {
-      const user = await ReactionVote.get(req.params.id)
-      res.status(OK).json(user)
-    } catch (err) {
-      next(err)
-    }
-  })
-  .put(async (req, res, next) => {
-    try {
-      const updatedReactionVote = await ReactionVote.update({ id: req.params.id, reactionVote: req.body })
-      res.status(OK).json(updatedReactionVote)
-    } catch (err) {
-      next(err)
-    }
-  })
-  .delete(async (req, res, next) => {
-    try {
-      await ReactionVote.remove(req.params.id)
-      res.status(OK).json({ id: req.params.id })
-    } catch (err) {
-      next(err)
-    }
-  })
+  .get(
+    isLoggedIn,
+    isAdmin,
+    async (req, res, next) => {
+      try {
+        const user = await ReactionVote.get(req.params.id)
+        res.status(OK).json(user)
+      } catch (err) {
+        next(err)
+      }
+    })
+  .put(
+    isLoggedIn,
+    isAdmin,
+    async (req, res, next) => {
+      try {
+        const updatedReactionVote = await ReactionVote.update({ id: req.params.id, reactionVote: req.body })
+        res.status(OK).json(updatedReactionVote)
+      } catch (err) {
+        next(err)
+      }
+    })
+  .delete(
+    isLoggedIn,
+    isAdmin,
+    async (req, res, next) => {
+      try {
+        await ReactionVote.remove(req.params.id)
+        res.status(OK).json({ id: req.params.id })
+      } catch (err) {
+        next(err)
+      }
+    })
 
 module.exports = router
